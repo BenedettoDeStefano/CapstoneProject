d5cfe4afc1e4595a70738c8824d8c8aecdf72ef8
Benedetto <129066047+BenedettoDeStefano@users.noreply.github.com>
2023-09-05 11:49:12 +0200

    Aggiunta annotazione Repository su event

--- Capstone-Project/src/main/java/CapstoneProject/Repository/EventRepository.java
+++ Capstone-Project/src/main/java/CapstoneProject/Repository/EventRepository.java
@@ -1,9 +1,11 @@
 package CapstoneProject.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import CapstoneProject.Entities.Event;
 
+@Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
 
 }
