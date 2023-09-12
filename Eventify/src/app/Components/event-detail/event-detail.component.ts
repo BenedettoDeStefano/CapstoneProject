c08598abb28a0d797e41e78338bfac8b60773ad4
Benedetto <129066047+BenedettoDeStefano@users.noreply.github.com>
2023-09-12 14:46:58 +0200

    Prenota

--- Eventify/src/app/Components/event-detail/event-detail.component.ts
+++ Eventify/src/app/Components/event-detail/event-detail.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { EventService } from 'src/app/Service/event.service'
 import { Review } from 'src/app/Models/review';
 import { ReviewService } from 'src/app/Service/review.service';
+import { ReservationService } from 'src/app/Service/reservation.service';
+import { UserService } from 'src/app/Service/user.service';
 
 @Component({
   selector: 'app-event-detail',
@@ -12,9 +14,9 @@ import { ReviewService } from 'src/app/Service/review.service';
 export class EventDetailComponent implements OnInit {
 
   eventDetails: any;
-  reviews: Review[] = [];
-
-  constructor(private route: ActivatedRoute,private eventService: EventService, private reviewService: ReviewService) { }
+  reviews:  Review[] = [];
+  currentUser: any;
+  constructor(private route: ActivatedRoute,private eventService: EventService, private reviewService: ReviewService, private reservationService:ReservationService, private userService: UserService) { }
 
 
   ngOnInit(): void {
@@ -28,8 +30,27 @@ export class EventDetailComponent implements OnInit {
               this.reviewService.getReviewsByEventId(eventId).subscribe((reviewsList) => {
                   this.reviews = reviewsList;
               });
+              this.userService.getCurrentUserInfo().subscribe(user => {
+                this.currentUser = user;
+            });
           }
       });
+  }
+
+  reserve(): void {
+    if (!this.currentUser) {
+      console.error('User is not logged in!');
+      return;
+    }
+    const payload = {
+      userId: this.currentUser.id,
+      eventId: this.eventDetails.id
+    };
+    this.reservationService.reserveTicket(payload).subscribe(response => {
+      console.log('Reservation confirmed:', response);
+    }, error => {
+      console.error('Error occurred:', error);
+    });
   }
 
 }
